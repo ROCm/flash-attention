a27560121a2e2befe84bb4b58c510e2f52312500
Michael <micmelesse@gmail.com>
2025-04-30 21:59:16 -0500

    remove env var

--- flash_attn/flash_attn_triton_amd/fwd_prefill.py
+++ flash_attn/flash_attn_triton_amd/fwd_prefill.py
@@ -1,4 +1,3 @@
-import os
 import torch
 import triton
 import triton.language as tl
@@ -235,20 +234,6 @@ def get_autotune_configs():
                 num_stages=1,
                 num_warps=4,
             )
-
-        # BEST_CONFIG = os.environ.get('BEST_CONFIG', '0').lower() in ('1', 'true', 'yes')
-        # if BEST_CONFIG:
-        #     default_config = triton.Config(
-        #         {"BLOCK_M": 128, "BLOCK_N": 128, "waves_per_eu": 2, "PRE_LOAD_V": False},
-        #         num_stages=1,
-        #         num_warps=4,
-        #     )
-        # else:
-        #     default_config = triton.Config(
-        #         {"BLOCK_M": 64, "BLOCK_N": 64, "waves_per_eu": 1, "PRE_LOAD_V": False},
-        #         num_stages=1,
-        #         num_warps=4,
-        #     )
         
         return [
             default_config
