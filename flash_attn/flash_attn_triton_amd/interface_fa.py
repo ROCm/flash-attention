117b25d00aacadec2f602a262d26e3456192edf1
Michael <micmelesse@gmail.com>
2025-06-19 14:34:00 -0500

    clean up fused bwd

--- flash_attn/flash_attn_triton_amd/interface_fa.py
+++ flash_attn/flash_attn_triton_amd/interface_fa.py
@@ -3,7 +3,7 @@ import os
 from .fwd_prefill import attention_prefill_forward_triton_impl
 from .bwd_prefill import attention_prefill_backward_triton_impl
 from .bwd_prefill_split import attention_prefill_backward_triton_split_impl
-from .bwd_prefill_fused import _flash_attn_backward as attention_prefill_backward_triton_fused_impl
+from .bwd_prefill_fused import attention_prefill_backward_triton_fused_impl
 from .bwd_prefill_onekernel import attention_prefill_backward_triton_split_oneKernel_impl
 from .fwd_decode import attention_decode_forward_triton_impl
 from .fwd_ref import attention_forward_pytorch_ref_impl, attention_decode_forward_ref_impl
